23ac6cf0b8ae20d88bbdb0ba0866a42d2f72ddb0
Jaykishan Kotak <jaykishankotak@gmail.com>
2022-09-26 16:22:17 +0530

    MongoDB Issues Fixed

--- server/database/mdb.js
+++ server/database/mdb.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
 
 const MongoConnection = async (username, password) => {
-    const URL = `mongodb://user:1234@ac-k17njhs-shard-00-00.3p91lls.mongodb.net:27017,ac-k17njhs-shard-00-01.3p91lls.mongodb.net:27017,ac-k17njhs-shard-00-02.3p91lls.mongodb.net:27017/PROJECT0?ssl=true&replicaSet=atlas-90997p-shard-0&authSource=admin&retryWrites=true&w=majority`
-
+    const URL = `mongodb://localhost:27017/Curd`
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
         console.log('MongoDB Connected...');
